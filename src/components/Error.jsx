260a51422056a3e9724a37a4e33058a1e9ecf00c
RicardoYone <xrikardox19@hotmail.com>
2022-07-27 22:10:21 -0500

    3

---
+++ src/components/Error.jsx
@@ -0,0 +1,13 @@
+import error from "../img/error.PNG"
+
+import "../styles/Error.css"
+
+const Error=()=>{
+    return(
+        <div>
+            <img src={error} alt="" />
+        </div>
+    )
+}
+
+export default Error;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import Login from './components/Login';
 import Register from './components/Register';
-
+import Pagos from './components/Pagos';
+import Error from "./components/Error"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Register/>
-    <Login/>
+    <Error/>
   </React.StrictMode>
 );
 
